6b6afc69ce6ff6ad5acbb1c991f2c8b15ab56fa3
권세명 <3pkwon@gmail.com>
2023-04-14 21:57:36 +0900

    json 미들웨어 추가 & todo 생성 구현

--- todolist_1/backend/routes/todo.js
+++ todolist_1/backend/routes/todo.js
@@ -3,10 +3,17 @@ let todoData = require("../todoData.json");
 
 const router = express.Router();
 
+// todolist 전체 조회
 router.get("/", (req, res) => {
   console.log(todoData);
 
   res.json(todoData);
+});
+
+// todolist 생성
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("임시로 투두생성");
 });
 
 module.exports = router;
