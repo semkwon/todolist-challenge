342b5e934ec395d8c34bdb4f531ed2d1a6283f90
권세명 <3pkwon@gmail.com>
2023-04-14 21:09:51 +0900

    todo 데이터, todo 라우팅, 데이터 조회

--- todolist_1/backend/app.js
+++ todolist_1/backend/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const todoRouter = require("./routes/todo");
 const app = express();
 const port = 3010;
+
+app.use("/todo", todoRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!!🚀");
