a6ae58c39017c54ca82bfc2c3d97c951e333f1ee
권세명 <3pkwon@gmail.com>
2023-04-15 18:29:50 +0900

    todo 완료여부

--- todolist_1/backend/routes/todo.js
+++ todolist_1/backend/routes/todo.js
@@ -20,4 +20,28 @@ router.post("/", (req, res) => {
   res.json(todoData);
 });
 
+// todolist 완료 여부
+router.put("/done/:id", (req, res) => {
+  const { id } = req.params;
+
+  todoData[parseInt(id)] = {
+    title: todoData[parseInt(id)].title,
+    desc: todoData[parseInt(id)].desc,
+    isDone: !todoData[parseInt(id)].isDone,
+  };
+
+  console.log(todoData);
+  res.json(todoData);
+
+  // 왜 이렇게 하면 안될까??
+  // let parseIntId = todoData[parseInt(id)];
+  // console.log(todoData[parseInt(id)]);
+
+  // parseIntId = {
+  //   title: parseIntId.title,
+  //   desc: parseIntId.desc,
+  //   isDone: !parseIntId.isDone,
+  // };
+});
+
 module.exports = router;
