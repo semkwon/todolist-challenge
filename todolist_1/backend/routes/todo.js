37713262246f2e9dcd6e8ded6cc7e7796335e32c
권세명 <3pkwon@gmail.com>
2023-04-15 22:59:04 +0900

    todo 수정 - UPDATE

--- todolist_1/backend/routes/todo.js
+++ todolist_1/backend/routes/todo.js
@@ -20,6 +20,29 @@ router.post("/", (req, res) => {
   res.json(todoData);
 });
 
+// todo 수정(update)
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, desc } = req.body;
+
+  if (parseInt(id) >= todoData.length) {
+    res.status(400).json({ error: "수정할 수 없는 ID 입니다." });
+  }
+
+  if (!title && !desc) {
+    res.status(400).json({ erro: "title or desc 둘 중 하나는 있어야 합니다." });
+  }
+
+  todoData[parseInt(id)] = {
+    title: title ? title : todoData[parseInt(id)].title,
+    desc: desc ? desc : todoData[parseInt(id)].desc,
+    isDone: todoData[parseInt(id)].isDone,
+  };
+
+  console.log(todoData);
+  res.json(todoData);
+});
+
 // todolist 완료 여부
 router.put("/done/:id", (req, res) => {
   const { id } = req.params;
