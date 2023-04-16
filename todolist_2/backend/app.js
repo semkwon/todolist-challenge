1a4afae04ce9d75e98a505e3a025f3206dee3ec4
권세명 <3pkwon@gmail.com>
2023-04-16 19:20:00 +0900

    TODOLIST_2 Todo Routing

---
+++ todolist_2/backend/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const todoRouter = require("./routes/todo");
+const app = express();
+const port = 3010;
+
+app.use(express.json());
+app.use("/todo", todoRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!! 🚀🚀");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port} 🚀🚀🚀`);
+});
---
+++ todolist_2/backend/routes/todo.js
@@ -0,0 +1,10 @@
+const express = require("express");
+let todoData = require("../todoData.json");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  console.log(todoData);
+  res.json(todoData);
+});
+
+module.exports = router;
