1218a5362ea7f5e8daa88148f61cc51c77b3a0b3
권세명 <3pkwon@gmail.com>
2023-04-16 22:16:16 +0900

    todolist 생성

--- todolist_2/backend/routes/todo.js
+++ todolist_2/backend/routes/todo.js
@@ -2,12 +2,14 @@ const express = require("express");
 let todoData = require("../todoData.json");
 const router = express.Router();
 
+// 전체 todolist 조회
 router.get("/", (req, res) => {
   console.log(todoData);
 
   res.json(todoData);
 });
 
+// 특정 todolist 조회
 router.get("/:id", (req, res) => {
   const { id } = req.params;
 
@@ -16,6 +18,22 @@ router.get("/:id", (req, res) => {
   }
 
   res.json(todoData[parseInt(id)]);
+});
+
+// todolist 생성
+router.post("/", (req, res) => {
+  const { title, desc } = req.body;
+
+  if (!title || !desc) {
+    return res
+      .status(400)
+      .json({ error: "title과 desc을 모두 입력해야 합니다." });
+  }
+
+  todoData.push({ title, desc, isDone: false });
+
+  console.log(todoData);
+  res.json(todoData);
 });
 
 module.exports = router;
